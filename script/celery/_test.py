470b2f646d1223a16607836493629d9fc3dc6e1b
weizhou <wei.zhou@dianjoy.com>
2016-10-14 14:05:05 +0800

    pusher.py

--- script/celery/_test.py
+++ script/celery/_test.py
@@ -1,8 +1,7 @@
-from pusher import *
-sum_test.delay('3')
-# print(a.backend)
-# a.wait()
-# print(a.result)
+from tasks import *
+a = sum_test.delay(num=3)
+a.wait()
+print(a.result)
 #
 # a = sum_test.delay('q3')
 # print(a.result)
--- script/celery/tasks.py
+++ script/celery/tasks.py
@@ -21,7 +21,6 @@ except ImportError:
     print('celeryconfig not found, using %s' % redis_url)
     app = Celery(APP_NAME, broker=redis_url, )  # backend=redis_url
 
-
 def mkdir_p(path):
     try:
         os.makedirs(path)
@@ -29,32 +28,10 @@ def mkdir_p(path):
         if e.errno == errno.EEXIST:
             pass
 
-
-# http://docs.celeryproject.org/en/latest/userguide/tasks.html
-@app.task(bind=True)
-def run(self, args):
-    out_fn, err_fn = args.pop(0), args.pop(0)
-    for fn in out_fn, err_fn:
-        mkdir_p(os.path.dirname(os.path.abspath(fn)))
-    with open(out_fn, "w") as fo, open(err_fn, "w") as fe:
-        ret = subprocess.call(args, stdout=fo, stderr=fe)
-    LOGGER.info("stdout: %s" % out_fn)
-    LOGGER.info("stderr: %s" % err_fn)
-    r = {'id': self.request.id, 'rc': ret}
-    if ret:
-        # Exponential backoff + jitter
-        delay_base = app.conf.get('CUSTOM_RETRY_DELAY', 1)
-        delay = int(uniform(2, 4) ** self.request.retries * delay_base)
-        raise self.retry(countdown=delay, exc=Exception(r))
-    return r
-
-
-def main(argv):
-    if len(argv) < 4:
-        print(app.conf.humanize(with_defaults=False, censored=True))
-        sys.stderr.write('USAGE: python %s OUT ERR CMD\n' % argv[0])
-        sys.exit(2)
-    return run.delay(args=argv[1:])
+@app.task()
+def sum_test(num):
+    num = num + 1
+    return num
 
 
 if __name__ == "__main__":
