3eb1e7af1b42e50d75ecaf9d4c3bb6fb0c0877b5
weizhou <602068971@qq.com>
2016-10-16 00:45:43 +0800

    s

--- script/celery/_test.py
+++ script/celery/_test.py
@@ -1,7 +1,10 @@
 from tasks import *
-a = sum_test.delay(num=3)
-a.wait()
-print(a.result)
+
+for ii in range(1000):
+    a = sum_test.delay(num=3)
+
+
+
 #
 # a = sum_test.delay('q3')
 # print(a.result)
